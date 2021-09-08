9d0a49b77ae1fe97392fe73e3273d5841672a0d7
Rob Waller <rdwaller1984@gmail.com>
2021-09-08 15:01:39 +0100

    Created functional test for api/automata endpoint and implemented code.

--- index.php
+++ index.php
@@ -3,15 +3,21 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use AutomataApp\CellularAutomata;
 
 require __DIR__ . '/vendor/autoload.php';
 
 $app = AppFactory::create();
 
-$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
-    $name = $args['name'];
-    $response->getBody()->write("Hello, $name");
-    return $response;
+$app->get('/api/automata', function (Request $request, Response $response) {
+    $params = $request->getQueryParams();
+    $cells = $params['cells'];
+    $rule = $params['rule'];
+    $steps = $params['steps'];
+    $cellularAutomata = new CellularAutomata();
+    $response->getBody()->write($cellularAutomata->step($cells, $rule, $steps));
+    $response = $response->withStatus(200);
+    return $response->withHeader('Content-Type', 'application/json');
 });
 
 $app->run();
---
+++ tests/Functional/ApiTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\Functional;
+
+use PHPUnit\Framework\TestCase;
+use Dotenv\Dotenv;
+
+class APITest extends TestCase
+{
+    private string $baseUrl;
+
+    private string $port;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $dotenv = Dotenv::createImmutable(__DIR__ . '/../../');
+        $dotenv->safeLoad();
+
+        $this->baseUrl = $_SERVER['TEST_URL'] ?? 'http://localhost';
+        $this->port = $_SERVER['TEST_PORT'] ?? '80';
+    }
+
+    public function testGetAutomata(): void
+    {
+        $client = new \GuzzleHttp\Client(['port' => $this->port]);
+        $response = $client->request('GET', $this->baseUrl . '/api/automata?cells=010&rule=110&steps=1');
+
+        $this->assertSame(200, $response->getStatusCode());
+        $this->assertStringContainsString('application/json', $response->getHeaderLine('content-type'));
+
+        $body = json_decode($response->getBody());
+
+        $this->assertSame([[0, 1, 0], [1, 1, 0],], $body);
+    }
+}
