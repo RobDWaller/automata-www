122f80618b2ec3452c9b8485408df50cb27aed4f
Rob Waller <rdwaller1984@gmail.com>
2021-09-08 14:34:45 +0100

    Added step two test for CellularAutomata class and implemented working code.

--- src/CellularAutomata.php
+++ src/CellularAutomata.php
@@ -4,15 +4,25 @@ declare(strict_types=1);
 
 namespace AutomataApp;
 
+use Automata\CellsFactory;
+use Automata\RulesFactory;
+use Automata\Iterator;
+use Automata\Iterate;
+
 class CellularAutomata
 {
     public function step(string $initialCells, int $rule, int $steps): string
     {
-        return json_encode(
-            [
-                [0, 1, 0],
-                [1, 1, 0],
-            ]
+        $cellsFactory = new CellsFactory();
+        $rulesFactory = new RulesFactory(); 
+        $iterate = new Iterate();
+
+        $iterator = new Iterator(
+            $iterate,
+            $cellsFactory->create($initialCells),
+            $rulesFactory->create($rule)
         );
+
+        return $iterator->iterate($steps)->toJson();
     }
 }
--- tests/Unit/CellularAutomataTest.php
+++ tests/Unit/CellularAutomataTest.php
@@ -23,4 +23,22 @@ class CellularAutomataTest extends TestCase
             $result
         );
     }
+
+    public function testStepTwo(): void
+    {
+        $cellularAutomata = new CellularAutomata();
+
+        $result = $cellularAutomata->step('0101', 110, 2);
+
+        $this->assertJsonStringEqualsJsonString(
+            json_encode(
+                [
+                    [0, 1, 0, 1],
+                    [1, 1, 1, 1],
+                    [0, 0, 0, 0],
+                ]
+            ),
+            $result
+        );
+    }
 }
