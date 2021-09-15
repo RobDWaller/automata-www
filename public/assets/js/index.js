8c76a9e15ff5dcc3ffcb011c59d763057537dae3
Rob Waller <rdwaller1984@gmail.com>
2021-09-15 15:21:09 +0100

    Align api automata call with rule loaded to page.

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -2,9 +2,11 @@ window.onload = function(){
   function reqListener () {
     console.log(this.responseText);
   }
+
+  let rule = document.getElementById("rule").value;
   
   var oReq = new XMLHttpRequest();
   oReq.addEventListener("load", reqListener);
-  oReq.open("GET", "http://localhost:8080/api/automata?cells=101&steps=3&rule=110");
+  oReq.open("GET", `http://localhost:8080/api/automata?cells=000010000&steps=8&rule=${rule}`);
   oReq.send();
 }
